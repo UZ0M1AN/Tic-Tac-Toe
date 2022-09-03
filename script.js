749439cb9f9b2d2e6dc2dfb9b7eb4e9ebd040e65
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-09-03 15:57:40 +0100

    Finish all but AI component

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const log = (...a) => console.log(...a);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////// FACTORY FUNCTIONS ////////////////////////////////////////////////////////
-function eventFactory() {
-    
-}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// MODULE PATTERNS //////////////////////////////////////////////////////
@@ -18,13 +16,17 @@ const elemModule = (function () {
         p2: document.querySelector('#p2'),
         p1Label: document.querySelector('.player-1 > label'),
         p2Label: document.querySelector('.player-2 > label'),
-        ai: document.querySelector('.ai'),
+        gameMode: document.querySelector('.gamemode'),
         gameboard: document.querySelector('.gameboard'),
         gameCells: document.querySelectorAll('td'),
         p1Name: document.querySelector('.p1-name'),
         p2Name: document.querySelector('.p2-name'),
         newGameDiv: document.querySelector('.new-game'),
-        newGameBtn: document.querySelector('.btn--new'),   
+        newGameBtns: document.querySelectorAll('.btn--new'),   
+        winnerDisplay: document.querySelector('.overlay'),
+        okBtn: document.querySelector('.btn--ok'),
+        winner: document.querySelector('.winner'),
+        trophyIcon: document.querySelector('.trophy'),
     }
 })();
 
@@ -34,7 +36,7 @@ const varModule = (function() {
         gameboardArr: [
             ['', '', ''],
             ['', '', ''],
-            ['', '', ''],     
+            ['', '', ''],
         ],
     }
 })();
@@ -45,38 +47,74 @@ const eventsModule = (function() {
         elem.classList[func](cssClass);
     }
 
-    function _winPattern() {
-        // const arr = varModule.gameboardArr;
-        // const [c1, c2, c3] = [arr[0][0], arr[0][1], arr[0][2]];
-        // const [c4, c5, c6] = [arr[1][0], arr[1][1], arr[1][2]];
-        // const [c7, c8, c9] = [arr[2][0], arr[2][1], arr[2][2]];
-        // let [x, o] = [{}, {}]; 
+    function _threeInARow() {
+        const arr = varModule.gameboardArr;
+        const [c1, c2, c3] = [arr[0][0], arr[0][1], arr[0][2]];
+        const [c4, c5, c6] = [arr[1][0], arr[1][1], arr[1][2]];
+        const [c7, c8, c9] = [arr[2][0], arr[2][1], arr[2][2]];
 
-        // for (let i = 0; i < arr.length; i++)
-        //     for (let j = 0; j < arr[i].length; j++) {
-        //         if (arr[i][j] == 'x') x[i] = j;
-        //         if (arr[i][j] == 'o') o[i] = j;
-        //     }
-        
-        // const keysX = Object.keys(x);
-        // const diffX = Object.keys(x).map((key, i, arr) => +arr[i+1] - +key)
-        // return diffX
-        
-        // return [_equality(c1,c2,c3), _equality(c4,c5,c6), _equality(c7,c8,c9), _equality(c1,c4,c7), _equality(c2,c5,c8), _equality(c3,c6,c9), _equality(c1,c5,c9), _equality(c3,c5,c7)].find(Boolean);
+        return (
+            _equal(c1, c2, c3) ? ['0-0', '0-1', '0-2']
+            : _equal(c4, c5, c6) ? ['1-0', '1-1', '1-2']
+            : _equal(c7, c8, c9) ? ['2-0', '2-1', '2-2']
+            : _equal(c1, c4, c7) ? ['0-0', '1-0', '2-0']
+            : _equal(c2, c5, c8) ? ['0-1', '1-1', '2-1']
+            : _equal(c3, c6, c9) ? ['0-2', '1-2', '2-2']
+            : _equal(c1, c5, c9) ? ['0-0', '1-1', '2-2']
+            : _equal(c3, c5, c7) ? ['0-2', '1-1', '2-0']
+            : false
+        );
+                
+    }
+    
+    function _equal(a, b, c) {
+        return (a == 'x' && b == 'x' && c == 'x') || (a == 'o' && b == 'o' && c == 'o');
     }
 
-    // function _equality(...val) {
-    //     const result = val.every(v => v == 'x') || val.every(v => v == 'o');
-    //     return result ? val : false;
-    // }
+    function _winGame(datacells = _threeInARow()) {
+        if (!datacells) return;
+
+        // Style the winning row
+        const cells = [...elemModule.gameCells].filter(gamecell => datacells.includes(gamecell.dataset.cell));
+        cells.forEach(cell => {
+            _changeClass(cell, 'add', 'win');
+        })
+
+        // Display Winner 
+        _changeClass(elemModule.winnerDisplay, 'remove', 'no-display');
+        elemModule.winner.textContent = `${elemModule[elemModule.p1Name.className.includes('active') ? 'p2Name' : 'p1Name'].textContent} wins!`;
+        elemModule.trophyIcon.innerHTML = '<i class="fa-solid fa-trophy"></i>';
+
+        // Stop Game
+        elemModule.gameCells.forEach(gameCell => {
+            gameCell.removeEventListener('click', eventsModule.displayXAndO);
+        })
+    }
+
+    function _drawGame() {
+        // Display Draw
+        _changeClass(elemModule.winnerDisplay, 'remove', 'no-display');
+        elemModule.winner.textContent = 'It\'s a tie!';
+        elemModule.trophyIcon.innerHTML = '<i class="fa-regular fa-handshake"></i>';
+    }
+
+    //////////////////////////////////////
+
 
     // Public Methods
     function startGame() {
+        // Add click event handler to game cells
+        elemModule.gameCells.forEach(gameCell => {
+            gameCell.addEventListener('click', eventsModule.displayXAndO);
+        })
+
+        // Set player names on gameboard
         const player1 = elemModule.p1.value || 'Uzoma';
         const player2 = elemModule.p2.className == 'no-display' ? 'Robot' : elemModule.p2.value || 'Mary';   
         elemModule.p1Name.textContent = player1;
         elemModule.p2Name.textContent = player2;
 
+        // Display gameboard and 'New Game' button, hide input fields
         _changeClass(elemModule.gameboard, 'remove', 'no-display');
         _changeClass(elemModule.newGameDiv, 'remove', 'no-display');
         _changeClass(elemModule.form, 'add', 'no-display');
@@ -85,9 +123,11 @@ const eventsModule = (function() {
         elemModule.p1.value = elemModule.p2.value = '';
     }
 
-    function changeForm(e) {
+    function changeToAIMode(e) {
+        // Hide Player 2 input field
         _changeClass(elemModule.p2, 'toggle', 'no-display');
 
+        // Toggle text content of Players' labels and Game Mode
         if (elemModule.p2Label.textContent) {
             elemModule.p2Label.textContent = '';
             elemModule.p1Label.textContent = 'Player';
@@ -112,14 +152,22 @@ const eventsModule = (function() {
         
         e.target.textContent = varModule.counter % 2 ? 'o' : 'x';
         varModule.gameboardArr[i[0]][i.slice(-1)] = e.target.textContent;
-        // log(varModule.gameboardArr);
-        // log(_winPattern())
+
+        // Check for win
+        if (_threeInARow()) _winGame();
+
+        // Check for tie
+        if ([...elemModule.gameCells].every(gamecell => gamecell.textContent != '')) _drawGame();
 
         // Update counter
         varModule.counter++;
     }
 
     function newGame() {
+        // Close winner display if it's open
+        closeWinnerDisplay()
+
+        // Display form and hide gameboard and new game button
         _changeClass(elemModule.gameboard, 'add', 'no-display');
         _changeClass(elemModule.newGameDiv, 'add', 'no-display');
         _changeClass(elemModule.form, 'remove', 'no-display');
@@ -127,31 +175,38 @@ const eventsModule = (function() {
         // Clear Gamebaord in DOM...
         elemModule.gameCells.forEach(gameCell => {
             gameCell.textContent = '';
+            _changeClass(gameCell, 'remove', 'win');
         })
 
         // ...and in Array
         varModule.gameboardArr = varModule.gameboardArr.map(x => ['', '', '']);
 
+        // Make Player 1 the active player
+        _changeClass(elemModule.p2Name, 'remove', 'active');
+        _changeClass(elemModule.p1Name, 'add', 'active');
+
         // Reset counter
         varModule.counter = 0;
+    }
+
+    function closeWinnerDisplay() {
+        _changeClass(elemModule.winnerDisplay, 'add', 'no-display');
     }
 
 
     return {
         startGame,
-        changeForm,
+        changeToAIMode,
         displayXAndO,
         newGame,
+        closeWinnerDisplay,
     }
 })();
 
 
 const DOMModule = (function() {
     elemModule.startBtn.addEventListener('click', eventsModule.startGame);
-    elemModule.ai.addEventListener('click', eventsModule.changeForm);
-    elemModule.newGameBtn.addEventListener('click', eventsModule.newGame);
-    
-    elemModule.gameCells.forEach(gameCell => {
-        gameCell.addEventListener('click', eventsModule.displayXAndO);
-    })
+    elemModule.gameMode.addEventListener('click', eventsModule.changeToAIMode);
+    elemModule.newGameBtns.forEach(newGameBtn => newGameBtn.addEventListener('click', eventsModule.newGame));
+    elemModule.okBtn.addEventListener('click', eventsModule.closeWinnerDisplay);
 })();
